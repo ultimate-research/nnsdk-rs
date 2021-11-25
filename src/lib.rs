f3140762e293f2bc016a088488d1b73a959d0f7a
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-11-24 19:18:47 -0500

    Add OfflineWebSession::new
    Co-authored-by: Raytwo <raytwost@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -5155,14 +5155,24 @@ pub mod root {
                 use self::super::{ShowOfflineHtmlPageArg, OfflineHtmlPageReturnValue};
                 #[repr(C)]
                 #[derive(Debug)]
-                pub struct OfflineWebSessionImpl {
-                    // This should be edited to use a const usize instead, as the size varies between SDK version. Refer to GetWorkBufferSize to get the actual size.
-                    pub x: [u8;0xd0],
-                }
-                #[repr(C)]
-                #[derive(Debug)]
                 pub struct OfflineWebSession {
-                    pub inner_web_impl: *const OfflineWebSessionImpl,
+                    pub inner_web_impl: *const u8,
+                }
+                impl OfflineWebSession {
+                    pub fn new() -> Self {
+                        let web_impl_size = unsafe { GetWorkBufferSize() };
+                        let inner_web_impl =  alloc::vec![0u8; web_impl_size].leak().as_ptr() as *const u8;
+
+                        let session = Self {
+                            inner_web_impl
+                        };
+
+                        unsafe {
+                            Initialize(&session, inner_web_impl);
+                        }
+
+                        session
+                    }
                 }
                 extern "C" {
                     #[link_name = "\u{1}_ZN2nn3web17OfflineWebSession17GetWorkBufferSizeEv"]
@@ -5170,7 +5180,7 @@ pub mod root {
                 }
                 extern "C" {
                     #[link_name = "\u{1}_ZN2nn3web17OfflineWebSession10InitializeEPvm"]
-                    pub fn Initialize(session: *const OfflineWebSession, web_impl: *const OfflineWebSessionImpl);
+                    pub fn Initialize(session: *const OfflineWebSession, offline_web_impl: *const u8);
                 }
                 extern "C" {
                     #[link_name = "\u{1}_ZN2nn3web17OfflineWebSession5StartEPPNS_2os15SystemEventTypeERKNS0_22ShowOfflineHtmlPageArgE"]
