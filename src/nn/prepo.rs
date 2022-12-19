30d5d6322941c2b47559d5e82a11e7de361abeab
jozz024 <74272560+jozz024@users.noreply.github.com>
2022-12-19 13:49:17 -0600

    Make the prepo bindings easier to use

--- src/nn/prepo.rs
+++ src/nn/prepo.rs
@@ -3,9 +3,14 @@ use self::super::root;
 #[repr(C)]
 pub struct PlayReport {
     pub m_EventName: [u8; 32usize],
-    pub m_Buff: *mut u8,
-    pub m_BuffLength: root::size_t,
-    pub m_End: u64,
+    pub m_Buff: *const u8,
+    pub m_BuffLength: usize,
+    pub m_End: usize,
+}
+
+#[repr(C)]
+pub struct Any64BitId {
+    pub id: i64
 }
 
 extern "C" {
@@ -14,99 +19,109 @@ extern "C" {
         this: *mut PlayReport,
         arg1: *const u8,
     ) -> root::Result;
-}
-extern "C" {
+
     #[link_name = "\u{1}_ZN2nn5prepo10PlayReport9SetBufferEv"]
     pub fn PlayReport_SetBuffer(this: *mut PlayReport)
         -> root::Result;
-}
-extern "C" {
+
     #[link_name = "\u{1}_ZN2nn5prepo10PlayReport3AddEPKcl"]
     pub fn PlayReport_Add(
         this: *mut PlayReport,
-        arg1: *const u8,
-        arg2: i64,
+        key: *const u8,
+        value: i64,
     ) -> root::Result;
-}
-extern "C" {
+
     #[link_name = "\u{1}_ZN2nn5prepo10PlayReport3AddEPKcd"]
     pub fn PlayReport_Add1(
         this: *mut PlayReport,
-        arg1: *const u8,
-        arg2: f64,
+        key: *const u8,
+        value: f64,
     ) -> root::Result;
-}
-extern "C" {
+
     #[link_name = "\u{1}_ZN2nn5prepo10PlayReport3AddEPKcS3_"]
     pub fn PlayReport_Add2(
         this: *mut PlayReport,
-        arg1: *const u8,
-        arg2: *const u8,
+        key: *const u8,
+        value: *const u8,
     ) -> root::Result;
-}
-extern "C" {
+
+    #[link_name = "\u{1}_ZN2nn5prepo10PlayReport3AddEPKcRKNS0_10Any64BitIdE"]
+    pub fn PlayReport_Add3(
+            this: *mut PlayReport,
+            key: *const u8,
+            value: Any64BitId,
+    ) -> root::Result;
+
     #[link_name = "\u{1}_ZN2nn5prepo10PlayReport4SaveEv"]
     pub fn PlayReport_Save(this: *mut PlayReport) -> root::Result;
-}
-extern "C" {
+
     #[link_name = "\u{1}_ZN2nn5prepo10PlayReport4SaveERKNS_7account3UidE"]
     pub fn PlayReport_Save1(
         this: *mut PlayReport,
-        arg1: *const root::nn::account::Uid,
+        uid: *const root::nn::account::Uid,
     ) -> root::Result;
-}
-extern "C" {
+
     #[link_name = "\u{1}_ZN2nn5prepo10PlayReportC1Ev"]
     pub fn PlayReport_PlayReport(this: *mut PlayReport);
 }
 impl PlayReport {
     #[inline]
-    pub unsafe fn SetEventId(&mut self, arg1: *const u8) -> root::Result {
-        PlayReport_SetEventId(self, arg1)
+    pub fn SetEventId(&mut self, arg1: *const u8) -> root::Result {
+        unsafe { PlayReport_SetEventId(self, arg1) }
     }
     #[inline]
-    pub unsafe fn SetBuffer(&mut self) -> root::Result {
-        PlayReport_SetBuffer(self)
+    pub fn SetBuffer(&mut self) -> root::Result {
+        unsafe { PlayReport_SetBuffer(self) }
     }
     #[inline]
-    pub unsafe fn Add(
+    pub fn AddLong(
         &mut self,
-        arg1: *const u8,
-        arg2: i64,
+        key: *const u8,
+        value: i64,
     ) -> root::Result {
-        PlayReport_Add(self, arg1, arg2)
+        unsafe { PlayReport_Add(self, key, value) }
     }
     #[inline]
-    pub unsafe fn Add1(
+    pub fn AddDouble(
         &mut self,
-        arg1: *const u8,
-        arg2: f64,
+        key: *const u8,
+        value: f64,
     ) -> root::Result {
-        PlayReport_Add1(self, arg1, arg2)
+        unsafe { PlayReport_Add1(self, key, value) }
     }
     #[inline]
-    pub unsafe fn Add2(
+    pub fn AddString(
         &mut self,
-        arg1: *const u8,
-        arg2: *const u8,
+        key: *const u8,
+        value: *const u8,
     ) -> root::Result {
-        PlayReport_Add2(self, arg1, arg2)
+        unsafe { PlayReport_Add2(self, key, value) }
     }
     #[inline]
-    pub unsafe fn Save(&mut self) -> root::Result {
-        PlayReport_Save(self)
-    }
-    #[inline]
-    pub unsafe fn Save1(
+    pub fn AddAny64BitID(
         &mut self,
-        arg1: *const root::nn::account::Uid,
+        key: *const u8,
+        value: Any64BitId,
     ) -> root::Result {
-        PlayReport_Save1(self, arg1)
+        unsafe { PlayReport_Add3(self, key, value) }
     }
     #[inline]
-    pub unsafe fn new() -> Self {
-        let mut __bindgen_tmp = ::core::mem::MaybeUninit::uninit();
-        PlayReport_PlayReport(__bindgen_tmp.as_mut_ptr());
-        __bindgen_tmp.assume_init()
+    pub fn Save(&mut self) -> root::Result {
+        unsafe { PlayReport_Save(self) }
+    }
+    #[inline]
+    pub fn SaveWithAccount(
+        &mut self,
+        uid: *const root::nn::account::Uid,
+    ) -> root::Result {
+        unsafe { PlayReport_Save1(self, uid) }
+    }
+    #[inline]
+    pub fn new() -> Self {
+        unsafe {
+            let mut __bindgen_tmp = ::core::mem::MaybeUninit::uninit();
+            PlayReport_PlayReport(__bindgen_tmp.as_mut_ptr());
+            __bindgen_tmp.assume_init()
+        }
     }
 }
