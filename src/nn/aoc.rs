f905b451f38636f29c173fc5244a7d89659307f7
Zarek Syed <zarek.syed@gmail.com>
2023-02-16 17:35:29 -0500

    Add nn::aoc (#37)
    * Add nn::aoc::ListAddOnContent()
    
    * Add nn::aoc::CountAddOnContent()

---
+++ src/nn/aoc.rs
@@ -0,0 +1,12 @@
+#[allow(unused_imports)]
+use self::super::root;
+
+extern "C" {
+    #[link_name = "\u{1}_ZN2nn3aoc17CountAddOnContentEv"]
+    pub fn CountAddOnContent();
+}
+
+extern "C" {
+    #[link_name = "\u{1}_ZN2nn3aoc16ListAddOnContentEPiii"]
+    pub fn ListAddOnContent(out_indices: *mut i32, offset: i32, count: i32);
+}
